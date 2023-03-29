956f49a71359fc2b4e96218661fe0296484629ff
Ahmed Reda <areda.gad@gmail.com>
2023-03-29 04:51:52 +0200

    interviewer test

---
+++ www/js/lang.js
@@ -0,0 +1,11 @@
+var lang = "ar";
+
+const home = document.getElementById("home-icon");
+const popular = document.getElementById("popular-icon");
+const settings = document.getElementById("settings-icon");
+
+if (lang == "ar") {
+  home.innerHTML = "الرئيسية";
+  popular.innerHTML = "الأشهر";
+  settings.innerHTML = "الاعدادات";
+}
